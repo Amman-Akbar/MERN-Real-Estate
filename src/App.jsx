ab1cd85d507c3779f415777ab07d417107d183aa
Amman-Akbar <amman2002akbar@gmail.com>
2023-11-11 21:25:05 +0500

    Completed Setting the Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import { Routes , Route} from 'react-router-dom'
+import Home from './Routes/Home'
+import About from './Routes/About'
+import Login from './Routes/Login'
+import SignOut from './Routes/SignOut'
+import Profile from './Routes/Profile'
 
 const App = () => {
   return (
-    <h1 className="text-5xl font-bold">
-      Hello world!
-    </h1>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="about" element={<About/>}/>
+      <Route path="profile" element={<Profile/>}/>
+      <Route path="login" element={<Login/>}/>
+      <Route path="signout" element={<SignOut/>}/>
+    </Routes>
   )
 }
 
